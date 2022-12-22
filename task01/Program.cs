7572c872f1b0badc6ca3f862b80dcb8fef698075
VladimirVysochanskiy <shunkar78@mail.ru>
2022-12-22 19:14:16 +0300

    Add Task01

---
+++ task01/Program.cs
@@ -0,0 +1,13 @@
+﻿// На пишите программу, которая генерирует случайное число из отрезка [10, 99] и выводит большую цифру из этого числа.
+int number = new Random().Next(10, 100);
+Console.WriteLine(number);
+int num1 = number / 10;
+int num2 = number % 10;
+if (num1 > num2)
+{
+    Console.WriteLine(num1);    
+}
+else
+{
+    Console.WriteLine(num2);
+}
